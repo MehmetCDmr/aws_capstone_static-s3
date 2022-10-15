a0311b14d668fbc5eb497581b6ad73e5bc31918d
MehmetCDmr <mcdisinit@gmail.com>
2022-10-15 13:19:37 -0400

    capstone project

---
+++ userdata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt-get update -y
+apt-get install git -y
+apt-get install python3 -y
+cd /home/ubuntu/
+TOKEN="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"
+git clone https://$TOKEN@github.com:MehmetCDmr/aws_capstone_matt.git
+cd /home/ubuntu/aws_capstone_matt
+apt install python3-pip -y
+apt-get install python3.7-dev libmysqlclient-dev -y
+pip3 install -r requirements.txt
+cd /home/ubuntu/aws_capstone_matt/src
+python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:80
