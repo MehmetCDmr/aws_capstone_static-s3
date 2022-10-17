ee2e63d928bf8b4cdd44d621402051bcaee481d5
Mehmet Can Demir <109488636+MehmetCDmr@users.noreply.github.com>
2022-10-17 17:17:30 -0400

    Update userdata.sh
    neeewww

--- userdata.sh
+++ userdata.sh
@@ -5,7 +5,6 @@ apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN="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"
 git clone https://$TOKENgit@github.com/MehmetCDmr/aws_capstone_matt.git
->>>>>>> c89b89d (new changes)
 cd /home/ubuntu/aws_capstone_matt
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
