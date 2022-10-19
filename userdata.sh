9605762dd18291f5139e64b1a6bdc66a71dcef9e
MehmetCDmr <mcdisinit@gmail.com>
2022-10-19 11:52:28 -0400

    tokeninfo

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+TOKEN="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"
 git clone https://$TOKEN@github.com/MehmetCDmr/aws_capstone_matt.git
 cd /home/ubuntu/aws_capstone_matt
 apt install python3-pip -y
