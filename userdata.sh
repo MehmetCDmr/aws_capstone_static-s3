70ad4f8217f175c56fee0cc2ab8f7d59b2a422ef
Mehmet Can Demir <109488636+MehmetCDmr@users.noreply.github.com>
2023-01-08 18:33:02 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="GITHUB TOKEN"
-git clone https://$TOKEN@github.com/MehmetCDmr/aws_capstone_matt.git
-cd /home/ubuntu/aws_capstone_matt
+TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+git clone https://$TOKEN@<YOUR GITHUB REPO URL>
+cd /home/ubuntu/<YOUR GITHUB REPO NAME>
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/aws_capstone_matt/src
+cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
